312da87d112330e7b21b002a5a4ed6e041afbdf1
Kirill <kirill-sosnyuk@yandex.ru>
2025-09-07 02:17:41 +0300

    file format option added

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,6 +2,10 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.nio.file.Path;
 
 @Command(
         name = "gendiff",
@@ -10,6 +14,28 @@ import picocli.CommandLine.Command;
         description = "Compares two configuration files and shows a difference."
 )
 public class App implements Runnable {
+    @Parameters(
+            index = "0",
+            description = "path to first file",
+            paramLabel = "filepath1"
+    )
+    private Path filePath1;
+
+    @Parameters(
+            index = "1",
+            description = "path to second file",
+            paramLabel = "filepath2"
+    )
+    private Path filePath2;
+
+    @Option(
+            names = {"-f", "--format"},
+            description = "output format [default: stylish]",
+            defaultValue = "stylish",
+            paramLabel = "format"
+    )
+    private String format;
+
 
     @Override
     public void run() {
